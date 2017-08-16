cf2b1cb8166fabd801f58b73410087588b916a34
aliaimviz <ali@aimviz.com>
2017-08-16 18:00:05 +0500

    mzz

--- app/City.php
+++ app/City.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     //
+    protected $fillable = [
+        'city_name'
+    ];
+    public function garage(){
+        return $this->hasOne('App\Garage','city_id', 'id');
+    }
 }
